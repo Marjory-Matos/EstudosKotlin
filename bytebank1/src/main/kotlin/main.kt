9ff7b24b10eeea007cbecef7690c3b1c39e31203
Marjory <marjory.matos@ibm.com>
2022-08-24 09:41:39 -0300

    for/while

--- bytebank1/src/main/kotlin/main.kt
+++ bytebank1/src/main/kotlin/main.kt
@@ -1,24 +1,59 @@
 fun main() {
-    //val variavel que nao permite modificar o valor
-    //var varival que permite modificar o valor
-    //deve sempre inicializar a variavel
-    val titular = "Alex"
-    val numeroConta = 1000
-    var saldo = 0.0
-    saldo = 100 + 2.0
-    saldo += 200
-    saldo -= 0.0
 
-    //podemos utilizar a string templete que eh chamar a variavel dentro da string
-    println("Titular $titular")
-    println("Numero da conta $numeroConta")
-    println("Saldo da conta $saldo")
+    var i = 0
+    while(i < 5) {
+        val titular = "Alex $i"
+        val numeroConta = 1000 + i
+        var saldo = i + 10.0
 
-    if(saldo > 0.0){
+        println("Titular $titular")
+        println("Numero da conta $numeroConta")
+        println("Saldo da conta $saldo")
+        println()
+        i++
+    }
+
+        /*
+         for (i in 1..3) {
+
+            //val variavel que nao permite modificar o valor
+            //var varival que permite modificar o valor
+            //deve sempre inicializar a variavel
+            val titular = "Alex $i"
+            val numeroConta = 1000 + i
+            var saldo = i + 10.0
+
+
+            //podemos utilizar a string templete que eh chamar a variavel dentro da string
+            println("Titular $titular")
+            println("Numero da conta $numeroConta")
+            println("Saldo da conta $saldo")
+            println()
+           */
+        // testaCondicoes(saldo)
+
+        //for in soh permite quando o primeiro numero eh menor do que o ultimo da condicao
+        //quando queremos diminuir temos que usar o downTo
+        /*
+        for(i in 5 downTo 1 step 1){
+            println(i)
+            //se quisermos parar a operacao utilizamos o if com o break
+            if(i == 4){
+                break
+            }
+        }
+        */
+}
+
+
+
+fun testaCondicoes(saldo: Double) {
+
+    if (saldo > 0.0) {
         println("conta eh positiva")
-    }else if(saldo == 0.0){
+    } else if (saldo == 0.0) {
         println("conta eh neutra")
-    }else {
+    } else {
         println("conta eh negativa")
     }
 
@@ -41,6 +76,5 @@ fun main() {
         saldo == 0.0 -> println("conta eh neutra")
         else -> println("conta eh negativa")
     }
-
 
 }
