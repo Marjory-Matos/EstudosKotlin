f364b2e5764dbe01a9c7e579637f280f7b7a83fd
Marjory <marjory.matos@ibm.com>
2022-08-24 10:31:38 -0300

    classes e entidades

--- bytebank1/src/main/kotlin/main.kt
+++ bytebank1/src/main/kotlin/main.kt
@@ -1,5 +1,35 @@
 fun main() {
+    println("Bem vindo ao Bytebank")
 
+    //instancia da classe
+    //nao conseguiremos imprimir dessa forma por isso teremos que a colocar dentro de uma variavel
+    val contaAlex = Conta()
+    contaAlex.titular = "Alex"
+    contaAlex.numero = 1000
+    contaAlex.saldo = 200.0
+    println(contaAlex.titular)
+    println(contaAlex.numero)
+    println(contaAlex.saldo)
+
+    val contaFran = Conta()
+    contaFran.titular = "Fran"
+    contaFran.numero = 1001
+    contaFran.saldo = 300.0
+    println(contaFran.titular)
+    println(contaFran.numero)
+    println(contaFran.saldo)
+
+
+}
+
+//temos que criar a classe no mesmo nivel de arquivo
+class Conta {
+    var titular = ""
+    var numero = 0
+    var saldo = 0.0
+}
+
+fun testaLacos(){
     var i = 0
     while(i < 5) {
         val titular = "Alex $i"
@@ -13,39 +43,37 @@ fun main() {
         i++
     }
 
-        /*
-         for (i in 1..3) {
+    /*
+      for (i in 1..3) {
 
-            //val variavel que nao permite modificar o valor
-            //var varival que permite modificar o valor
-            //deve sempre inicializar a variavel
-            val titular = "Alex $i"
-            val numeroConta = 1000 + i
-            var saldo = i + 10.0
+         //val variavel que nao permite modificar o valor
+         //var varival que permite modificar o valor
+         //deve sempre inicializar a variavel
+         val titular = "Alex $i"
+         val numeroConta = 1000 + i
+         var saldo = i + 10.0
 
 
-            //podemos utilizar a string templete que eh chamar a variavel dentro da string
-            println("Titular $titular")
-            println("Numero da conta $numeroConta")
-            println("Saldo da conta $saldo")
-            println()
-           */
-        // testaCondicoes(saldo)
-
-        //for in soh permite quando o primeiro numero eh menor do que o ultimo da condicao
-        //quando queremos diminuir temos que usar o downTo
-        /*
-        for(i in 5 downTo 1 step 1){
-            println(i)
-            //se quisermos parar a operacao utilizamos o if com o break
-            if(i == 4){
-                break
-            }
-        }
+         //podemos utilizar a string templete que eh chamar a variavel dentro da string
+         println("Titular $titular")
+         println("Numero da conta $numeroConta")
+         println("Saldo da conta $saldo")
+         println()
         */
+    // testaCondicoes(saldo)
+
+    //for in soh permite quando o primeiro numero eh menor do que o ultimo da condicao
+    //quando queremos diminuir temos que usar o downTo
+    /*
+    for(i in 5 downTo 1 step 1){
+        println(i)
+        //se quisermos parar a operacao utilizamos o if com o break
+        if(i == 4){
+            break
+        }
+    }
+    */
 }
-
-
 
 fun testaCondicoes(saldo: Double) {
 
